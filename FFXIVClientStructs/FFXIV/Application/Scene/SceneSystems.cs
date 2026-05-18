e596cd13c7bbb4afa9f0e4297c0eb0b581ff3dae
Yokimitsuro <gonzalezciclo@gmail.com>
2026-05-19 00:29:13 +0200

    Map Application Scene systems: 70 structs (Vfx 22, Root 20, Command 12, Query 8, Controllers 5, System 3)

---
+++ FFXIVClientStructs/FFXIV/Application/Scene/SceneSystems.cs
@@ -0,0 +1,495 @@
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.Attributes;
+
+namespace FFXIVClientStructs.FFXIV.Application.Scene;
+
+[Rtti(".?AVDrawImage@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawImage
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA2A00 (3 vfuncs)
+}
+
+[Rtti(".?AVDrawImageWithMask@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawImageWithMask
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA2A10 (3 vfuncs)
+}
+
+[Rtti(".?AVDrawLine@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawLine
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA29C0 (3 vfuncs)
+}
+
+[Rtti(".?AVDrawRectangle@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawRectangle
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA29B0 (3 vfuncs)
+}
+
+[Rtti(".?AVDrawText@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawText
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA29D0 (3 vfuncs)
+}
+
+[Rtti(".?AVDrawingBase@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawingBase
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA288C (3 vfuncs)
+}
+
+[Rtti(".?AVDrawingCommand@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_DrawingCommand
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA2C8C (3 vfuncs)
+}
+
+[Rtti(".?AVFontManagerInterface@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_FontManagerInterface
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x0105E060 (2 vfuncs)
+}
+
+[Rtti(".?AVPopColor@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_PopColor
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA2A30 (3 vfuncs)
+}
+
+[Rtti(".?AVPopTransform@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_PopTransform
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA29F0 (3 vfuncs)
+}
+
+[Rtti(".?AVPushColor@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_PushColor
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA2A20 (3 vfuncs)
+}
+
+[Rtti(".?AVPushTransform@DrawParam@Window@Command@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Command_Window_DrawParam_PushTransform
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA29E0 (3 vfuncs)
+}
+
+[Rtti(".?AVCharaLightNode@Controllers@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Controllers_CharaLightNode
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEE128 (6 vfuncs)
+}
+
+[Rtti(".?AVRaptureCharacterController@Controllers@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Controllers_RaptureCharacterController
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEE17C (36 vfuncs)
+}
+
+[Rtti(".?AVRaptureCharacterProxy@Controllers@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Controllers_RaptureCharacterProxy
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEE210 (9 vfuncs)
+}
+
+[Rtti(".?AVRaptureConstraintController@Controllers@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Controllers_RaptureConstraintController
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEED6C (5 vfuncs)
+}
+
+[Rtti(".?AVRaptureScheduleController@Controllers@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Controllers_RaptureScheduleController
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEE23C (24 vfuncs)
+}
+
+[Rtti(".?AVOcclusionBoundary@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_OcclusionBoundary
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x010468D4 (2 vfuncs)
+}
+
+[Rtti(".?AVOcclusionBoundaryAABB@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_OcclusionBoundaryAABB
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x010468E0 (2 vfuncs)
+}
+
+[Rtti(".?AVOcclusionBoundaryLookAtAABB@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_OcclusionBoundaryLookAtAABB
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x01125974 (2 vfuncs)
+}
+
+[Rtti(".?AVOcclusionBoundaryMatrixAABB@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_OcclusionBoundaryMatrixAABB
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x01125968 (2 vfuncs)
+}
+
+[Rtti(".?AVOcclusionQueryHandle@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_OcclusionQueryHandle
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FBC518 (2 vfuncs)
+}
+
+[Rtti(".?AVOcclusionQueryNode@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_OcclusionQueryNode
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FBC500 (5 vfuncs)
+}
+
+[Rtti(".?AVQueryHandle@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_QueryHandle
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FBC4F8 (1 vfunc)
+}
+
+[Rtti(".?AVQueryNode@Query@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Query_QueryNode
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FBC4E0 (5 vfuncs)
+}
+
+[Rtti(".?AVQueue<ScenePacket>@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct QueueScenePacket
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F931C0 (1 vfunc)
+}
+
+[Rtti(".?AVRaptureActor@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureActor
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEA50C (160 vfuncs)
+}
+
+[Rtti(".?AVRaptureActorClipListener@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureActorClipListener
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB7AF4 (11 vfuncs)
+}
+
+[Rtti(".?AVRaptureActorContainer@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureActorContainer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE8ABC (11 vfuncs)
+}
+
+[Rtti(".?AVRaptureActorContainerMap@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureActorContainerMap
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FED594 (1 vfunc)
+}
+
+[Rtti(".?AVRaptureActorSchedulerListener@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureActorSchedulerListener
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEA4C8 (13 vfuncs)
+}
+
+[Rtti(".?AVRaptureActorSchedulerListenerBase@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureActorSchedulerListenerBase
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB7ABC (13 vfuncs)
+}
+
+[Rtti(".?AVRaptureFontData@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureFontData
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F931A0 (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureModelObject@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureModelObject
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x010653A4 (91 vfuncs)
+}
+
+[Rtti(".?AVRaptureModelObjectFactory@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureModelObjectFactory
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FEA500 (2 vfuncs)
+}
+
+[Rtti(".?AVRaptureReferenceResource@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureReferenceResource
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x01047240 (12 vfuncs)
+}
+
+[Rtti(".?AVRaptureResourceFileRoot@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureResourceFileRoot
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE981C (15 vfuncs)
+}
+
+[Rtti(".?AVRaptureResourceSceneGraph@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureResourceSceneGraph
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE96FC (28 vfuncs)
+}
+
+[Rtti(".?AVRaptureTextureResource@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureTextureResource
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F932D8 (3 vfuncs)
+}
+
+[Rtti(".?AVRaptureWorld@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureWorld
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB6F44 (11 vfuncs)
+}
+
+[Rtti(".?AVSceneModule@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct SceneModule
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F91DE8 (6 vfuncs)
+}
+
+[Rtti(".?AVSceneModuleInterface@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct SceneModuleInterface
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F91DCC (6 vfuncs)
+}
+
+[Rtti(".?AVScenePacket@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct ScenePacket
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F8CC14 (1 vfunc)
+}
+
+[Rtti(".?AVSceneThread@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct SceneThread
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F93168 (4 vfuncs)
+}
+
+[Rtti(".?AVSceneThreadImpl@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct SceneThreadImpl
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F931D4 (41 vfuncs)
+}
+
+[Rtti(".?AVBaseInstanceMemoryBalancer@MemoryBalancer@System@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct System_MemoryBalancer_BaseInstanceMemoryBalancer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB6398 (3 vfuncs)
+}
+
+[Rtti(".?AVBaseMemoryBalancer@MemoryBalancer@System@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct System_MemoryBalancer_BaseMemoryBalancer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB638C (2 vfuncs)
+}
+
+[Rtti(".?AVBaseResourceMemoryBalancer@MemoryBalancer@System@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct System_MemoryBalancer_BaseResourceMemoryBalancer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB63C8 (5 vfuncs)
+}
+
+[Rtti(".?AVVfxInstanceMemoryBalancer@MemoryBalancer@System@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct System_MemoryBalancer_VfxInstanceMemoryBalancer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB63A8 (3 vfuncs)
+}
+
+[Rtti(".?AVVfxResourceMemoryBalancer@MemoryBalancer@System@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct System_MemoryBalancer_VfxResourceMemoryBalancer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB63F0 (5 vfuncs)
+}
+
+[Rtti(".?AVGraphResource@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_GraphResource
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF55B0 (3 vfuncs)
+}
+
+[Rtti(".?AVIGraphResourceListener@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_IGraphResourceListener
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF5224 (3 vfuncs)
+}
+
+[Rtti(".?AVIRaptureSchEffectController@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_IRaptureSchEffectController
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF5418 (7 vfuncs)
+}
+
+[Rtti(".?AVIRaptureVfxActor@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_IRaptureVfxActor
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF52F4 (30 vfuncs)
+}
+
+[Rtti(".?AVIVfxGraphManager@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_IVfxGraphManager
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF538C (7 vfuncs)
+}
+
+[Rtti(".?AVIVfxRaptureParameterListener@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_IVfxRaptureParameterListener
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x010116D4 (3 vfuncs)
+}
+
+[Rtti(".?AVIQixFindResourceManager@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_IQixFindResourceManager
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x0105AF54 (5 vfuncs)
+}
+
+[Rtti(".?AVQixFindResourceManager@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_QixFindResourceManager
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x0105AF6C (5 vfuncs)
+}
+
+[Rtti(".?AVRaptureBaseReferenceResource@Resource@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_Resource_RaptureBaseReferenceResource
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE937C (11 vfuncs)
+}
+
+[Rtti(".?AVRaptureEffectResourceNodeFactory@Resource@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_Resource_RaptureEffectResourceNodeFactory
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE944C (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureLeafResourceNodeFactory@Resource@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_Resource_RaptureLeafResourceNodeFactory
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE948C (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureLeafinsResourceNodeFactory@Resource@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_Resource_RaptureLeafinsResourceNodeFactory
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE946C (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureModelResourceNodeFactory@Resource@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_Resource_RaptureModelResourceNodeFactory
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE94AC (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureTextureResourceNodeFactory@Resource@Qix@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_Qix_Resource_RaptureTextureResourceNodeFactory
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FE94CC (7 vfuncs)
+}
+
+[Rtti(".?AVAlphaOcclusionController@Occlusion@Controls@QixControl@RaptureQixControl@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_RaptureQixControl_QixControl_Controls_Occlusion_AlphaOcclusionController
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x01125950 (5 vfuncs)
+}
+
+[Rtti(".?AVRaptureSchEffectController@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_RaptureSchEffectController
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF5628 (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureVfxActor@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_RaptureVfxActor
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF54FC (31 vfuncs)
+}
+
+[Rtti(".?AVRaptureVfxActorContainer@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_RaptureVfxActorContainer
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF558C (1 vfunc)
+}
+
+[Rtti(".?AVVfxGraphManager@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_VfxGraphManager
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF55C0 (7 vfuncs)
+}
+
+[Rtti(".?AVVfxNoneCopyRaptureParameter@VfxRaptureParameter@Vfx@Scene@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct Vfx_VfxRaptureParameter_VfxNoneCopyRaptureParameter
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FF5584 (1 vfunc)
+}
+
