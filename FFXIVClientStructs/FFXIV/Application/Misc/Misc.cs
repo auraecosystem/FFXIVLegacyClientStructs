13b1fd65be6de7903b98928a4a6b471091509631
Yokimitsuro <gonzalezciclo@gmail.com>
2026-05-19 00:35:17 +0200

    Map Application Misc module: 19 structs (GameConfig, UserConfig, SystemConfig, ResourceConfig, FontModule, LogModule, MacroModule, SoundModule, TextureManager, StringTable)

---
+++ FFXIVClientStructs/FFXIV/Application/Misc/Misc.cs
@@ -0,0 +1,138 @@
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.Attributes;
+
+namespace FFXIVClientStructs.FFXIV.Application.Misc;
+
+[Rtti(".?AVAsyncWorkThread@AsyncWorkModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct AsyncWorkModule_AsyncWorkThread
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F909A8 (4 vfuncs)
+}
+
+[Rtti(".?AVAsyncWorkTaskInterface@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct AsyncWorkTaskInterface
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FA2C1C (3 vfuncs)
+}
+
+[Rtti(".?AVGameConfig@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct GameConfig
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A98 (2 vfuncs)
+}
+
+[Rtti(".?AVQueue@PAVAsyncWorkTaskInterface@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct PAVAsyncWorkTaskInterface_Queue
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A20 (1 vfunc)
+}
+
+[Rtti(".?AVRaptureFileEvent@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureFileEvent
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F93138 (7 vfuncs)
+}
+
+[Rtti(".?AVRaptureFontModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureFontModule
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F8CBD0 (8 vfuncs)
+}
+
+[Rtti(".?AVRaptureLogModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureLogModule
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F91434 (10 vfuncs)
+}
+
+[Rtti(".?AVRaptureMacroModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureMacroModule
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A30 (3 vfuncs)
+}
+
+[Rtti(".?AVRaptureSoundModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureSoundModule
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90F9C (9 vfuncs)
+}
+
+[Rtti(".?AVRaptureStringTable@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureStringTable
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90AAC (1 vfunc)
+}
+
+[Rtti(".?AVRaptureSupportModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureSupportModule
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A80 (1 vfunc)
+}
+
+[Rtti(".?AVRaptureSupportModuleCore@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureSupportModuleCore
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90AA4 (1 vfunc)
+}
+
+[Rtti(".?AVPcSearchCallback@RaptureSupportModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureSupportModule_PcSearchCallback
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F909E4 (3 vfuncs)
+}
+
+[Rtti(".?AVPcSearchDebugCallback@RaptureSupportModule@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureSupportModule_PcSearchDebugCallback
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F909F4 (3 vfuncs)
+}
+
+[Rtti(".?AVRaptureTextureManager@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct RaptureTextureManager
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00FB6D94 (2 vfuncs)
+}
+
+[Rtti(".?AVResourceConfig@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct ResourceConfig
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A14 (2 vfuncs)
+}
+
+[Rtti(".?AVConfigChangeEvent@ResourceConfig@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct ResourceConfig_ConfigChangeEvent
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90FC4 (2 vfuncs)
+}
+
+[Rtti(".?AVSystemConfig@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct SystemConfig
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A50 (2 vfuncs)
+}
+
+[Rtti(".?AVUserConfig@Misc@Application@@")]
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe struct UserConfig
+{
+    [FieldOffset(0x0000)] public nint VTable; // 0x00F90A6C (2 vfuncs)
+}
+
